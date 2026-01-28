5ad500f94bfbc1491b0c91e0f74acc808c59ee8e
Kaiser <kaisarmunna.km15@gmail.com>
2026-01-28 22:18:06 +0600

    deploy fix

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrandMission } from "./BrandMission";
 import { BrandPhilosophy } from "./BrandPhilosophy";
-import { ScrollRevealAnimation } from "../../helper/scrollRevealAnimation";
+import { ScrollRevealAnimation } from "../../helper/ScrollRevealAnimation";
 
 export const About = () => {
   return (
